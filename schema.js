6991726cb200ca18718d48ba945a5403331f39da
Taylor Allen <taylorallen0913@gmail.com>
2020-07-05 18:32:46 -0700

    Fixed code

--- schema.js
+++ schema.js
@@ -2,33 +2,27 @@ const axios = require('axios');
 const { GraphQLObjectType, GraphQLFloat, GraphQLList, GraphQLSchema, GraphQLString, GraphQLScalarType, GraphQLInt} = require('graphql');
 
 // Bitcoin data type
-const rateData = new GraphQLObjectType ({
-  name: 'Rates',
-  fields: () => ({
-    name: {type: GraphQLString},
-    rate: {type: GraphQLString}
-  })
-})
-const LaunchType = new GraphQLObjectType({
+const CurrencyDataType = new GraphQLObjectType({
   name: 'launch',
   fields: () => ({
-    AED: {type: rateData},
-    AFN: {type: rateData},
-    AMD: {type: rateData},
-    ANG: {type: rateData},
-    AOA: {type: rateData}
+    code: {type: GraphQLString},
+    symbol: {type: GraphQLString},
+    rate_float: {type: GraphQLFloat}
   })
 });
 
 //Root Query
-
 const RootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
     launchInfo: {
-      type: new GraphQLList(LaunchType),
-      resolve(parent, args) {
-        return axios.get('https://apiv2.bitcoinaverage.com/constants/exchangerates').then(res => res.data);
+      type: CurrencyDataType,
+      args: {
+        currency: {type: GraphQLString}
+      },
+      resolve: async(parent, args) => {
+        const coinData = await axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(res => res.data.bpi);
+        return coinData[args.currency];
       }
     }
   }
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
-const app = express();
+const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
-app.use('/graphql', graphqlHTTP({
-  schema: schema,
-  graphiql: true
-}));
 
-var PORT = process.env.PORT || 5000;
+const app = express();
+
+app.use('/graphql', 
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true
+  }),
+);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('Run server on localhost:' + PORT));
